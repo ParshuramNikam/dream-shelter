f56e0ee2b9e03844b9c442baa831f889fb311c18
Parshuram Nikam <parshunikam0612@gmail.com>
2022-02-09 20:47:27 +0530

    add login and signup page in feature1 branch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+} from "react-router-dom";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+import PageLayout from "./components/PageLayout";
+import Signup from "./components/Signup";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/">
+					{/* Page Layput already having navbar */}
+					<PageLayout>	
+						<div>This is Home - Put your component here!</div>
+					</PageLayout>
+				</Route>
+				<Route path="/login">
+					<Login />
+				</Route>
+				<Route path="/signup">
+					<Signup />
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
+
---
+++ src/components/Login.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+function Login() {
+
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const handleSubmit = () => {
+		if (email === "" || password === "") {
+			alert("Please!!! Enter all fields!");
+			return;
+		}
+
+		alert(`Email : ${email} | Password : ${password}`);
+	}
+
+	return (
+		<div className="flex items-center h-screen w-full font-sans bg-indigo-500">
+			<div className="bg-white shadow rounded lg:w-1/3  md:w-1/2 w-full p-10 md:mx-auto">
+				<p tabIndex={0} role="heading" aria-label="Login to your account" className="text-2xl font-extrabold leading-6 text-gray-800">
+					Login to your account
+				</p>
+				<p className="text-sm mt-4 font-medium leading-none text-gray-500">
+					Dont have account? &nbsp;{" "}
+					<Link to="/signup">
+						<span tabIndex={0} role="link" aria-label="Sign up here" className="text-sm font-bold leading-none underline text-indigo-900 cursor-pointer">
+							{" "}
+							Sign up here
+						</span>
+					</Link>
+				</p>
+				<button aria-label="Continue with google" role="button" className="focus:outline-none focus:ring-2 focus:ring-offset-1  py-2.5 px-4 border-2 rounded-lg border-indigo-700 flex items-center w-full mt-10">
+					<img src="/images/google.svg" />
+					<p className="text-base font-medium ml-4 text-gray-700">Continue with Google</p>
+				</button>
+
+				<div className="w-full flex items-center justify-between py-6">
+					<hr className="w-full bg-gray-400" />
+					<p className="text-base font-medium leading-4 px-2.5 text-gray-400">OR</p>
+					<hr className="w-full bg-gray-400  " />
+				</div>
+				<div>
+					<lable className="text-sm font-medium leading-none text-gray-800">Email</lable>
+					<input aria-label="enter email adress" role="input" type="email" className="bg-gray-200 border rounded focus:outline-none text-xs font-medium leading-none text-gray-800 py-3 w-full pl-3 mt-2"
+						value={email} onChange={(e) => setEmail(e.target.value)}
+					/>
+				</div>
+				<div className="mt-6  w-full">
+					<lable className="text-sm font-medium leading-none text-gray-800">Password</lable>
+					<div className="">
+						<input aria-label="enter Password" role="input" type="password" className="bg-gray-200 border rounded focus:outline-none text-xs font-medium leading-none text-gray-800 py-3 w-full pl-3 mt-2 pr-10"
+							value={password} onChange={(e) => setPassword(e.target.value)}
+						/>
+						<div class="float-right mt-1.5 flex items-center text-sm">
+							<input type="checkbox" name="passswordToggle" id="passswordToggle" class="px-1 mr-2 cursor-pointer h-3.5 w-3.5" />
+							<label htmlFor="passswordToggle">Show Password</label>
+						</div>
+					</div>
+				</div>
+				<div className="mt-10">
+					<button role="button" aria-label="create my account" className="focus:ring-2 focus:ring-offset focus:ring-indigo-700 text-sm font-semibold leading-none text-white focus:outline-none bg-indigo-700 border rounded-lg hover:bg-indigo-600 py-4 w-full"
+						onClick={handleSubmit}
+					>
+						Create my account
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Login;
